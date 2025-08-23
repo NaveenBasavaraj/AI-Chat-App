3a12e749ad4258a11ac45b8a91830e1437683ad2
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2025-08-23 13:15:31 +0000

    routing

--- reactchat/src/App.tsx
+++ reactchat/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, Route, RouteProvider } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
