43ae32a0646b4c5862668a70bc52d54481b0d41d
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2025-08-23 06:24:12 +0000

    a

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -42,10 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djchat',
+    'drf_spectacular',
     'rest_framework',
+    'djchat',
     'account',
-    'server'
+    'server',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': True,
+    # OTHER SETTINGS
+}
