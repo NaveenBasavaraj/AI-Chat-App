9aaf7203b30177d6542a4e82819fa792b930ec26
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2025-08-23 04:38:47 +0000

    model changes

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djchat',
-    'rest_framework'
+    'rest_framework',
+    'account'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "account.Account"
