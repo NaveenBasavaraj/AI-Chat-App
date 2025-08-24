fc0e70fcf688d8e28b07ea573fcc73f0b9271b33
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2025-08-24 02:46:10 +0000

    added server serializer

--- djchat/server/api/viewsets.py
+++ djchat/server/api/viewsets.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework import viewsets
 from server.models import Server
+from server.api.serializers import ServerSerializer
 # Create your views here.
 class ServerListViewset(viewsets.ViewSet):
     queryset = Server.objects.all()
@@ -9,3 +11,6 @@ class ServerListViewset(viewsets.ViewSet):
         category = request.query_params.get("category")
         if category:
             self.queryset.filter(category=category)
+        
+        serializer = ServerSerializer(self.queryset, many=True)
+        return Response(serializer.data)
