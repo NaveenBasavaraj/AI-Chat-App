d357c7b5c6649c73b3fabf5e8e5c827b36061fa1
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2025-08-24 04:02:52 +0000

    adding memeber and category filter

--- djchat/server/api/viewsets.py
+++ djchat/server/api/viewsets.py
@@ -10,14 +10,13 @@ class ServerListViewset(viewsets.ViewSet):
     def list(self, request):
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
-        by_user = request.query_params.get("by_user") == True
+        user_id = request.query_params.get("user_id")
         if category:
             self.queryset=self.queryset.filter(category=category)
+        if user_id:
+            user_id = request.user.id
+            self.queryset=self.queryset.filter(member=user_id)
         if qty:
             self.queryset=self.queryset[:int(qty)]
-        if by_user:
-            user_id = request.user.id
-            self.queryset=self.queryset.filter(memeber=user_id)
-        
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
