1a35391ca15d7c1e01ad27a093d7bdb01b4f7a15
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2025-08-24 03:18:40 +0000

    registered account/user model

--- djchat/account/admin.py
+++ djchat/account/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from server.models import Category, Server, Channel
+from account.models import Account
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Server)
 admin.site.register(Channel)
+admin.site.register(Account)
--- djchat/server/api/viewsets.py
+++ djchat/server/api/viewsets.py
@@ -9,8 +9,15 @@ class ServerListViewset(viewsets.ViewSet):
 
     def list(self, request):
         category = request.query_params.get("category")
+        qty = request.query_params.get("qty")
+        by_user = request.query_params.get("by_user") == True
         if category:
             self.queryset=self.queryset.filter(category=category)
+        if qty:
+            self.queryset=self.queryset[:int(qty)]
+        if by_user:
+            user_id = request.user.id
+            self.queryset=self.queryset.filter(memeber=user_id)
         
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
